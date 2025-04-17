5e0258c95b816266dd811a8e81cee9618cf79d95
Petrichor-08 <so309171@students.davnport.k12.ia.us>
2025-04-17 14:41:58 -0500

    added new stuff

--- StandardArrayAlgorithms.java
+++ StandardArrayAlgorithms.java
@@ -3,7 +3,7 @@ public class StandardArrayAlgorithms {
     {
       int max = 0;
       int min = 0;
-            int[] goals = {1, 2, 0, 3, 2, 4, 2, 1, 0, 2, 0, 1, 3, 2};
+      int[] goals = {1, 2, 0, 3, 2, 4, 2, 1, 0, 2, 0, 1, 3, 2};
             int[] goalCounter = new int[9];
           int sum = 0;
           for (int i = 0; i < goals.length; i++)
@@ -35,5 +35,42 @@ public class StandardArrayAlgorithms {
         {
             System.out.println("there were " + goalCounter[r] + " games with " + r + " goals" );
         }
+        Player[] players = {new Player("Alex", 12), new Player("Aiden", 13),
+        new Player("Bobbie", 18), new Player("Blaine", 20),
+        new Player("Chris", 15), new Player("Charlie", 15) };
+int o = 0;
+boolean hasValue = false;
+     for( hasValue = false; hasValue == true; o++)
+     {
+      if(players[o].getAge() >= 18)
+      {
+        hasValue = true;
+      }
+     }
+     if(hasValue == true)
+     {
+      System.err.println("a player was found that was over 18");
+     }
+     else
+     {
+      System.err.println("a player was not found that was over 18");
+     }
+    int j = 0;
+    boolean allHasValue = false;
+     for( allHasValue = false; j >= players.length; o++)
+     {
+      if(players[o].getAge() >= 21)
+      {
+        allHasValue = true;
+      }
+     }
+     if(allHasValue == true)
+     {
+      System.err.println("not all players are over 21");
+     }
+     else
+     {
+      System.err.println("all players are under 21");
+     }
     }
-}
+  }
